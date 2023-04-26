526fd19a594ad81339c38b62c076f4ac78895279
Sofia Lee <sofia819@gmail.com>
2023-04-26 00:41:07 -0400

    Add selection

--- timer-client/src/App.js
+++ timer-client/src/App.js
@@ -4,6 +4,7 @@ import { HubConnectionBuilder } from '@microsoft/signalr';
 const App = () => {
   const [connection, setConnection] = useState(null);
   const [timeUntil, setTimeUntil] = useState(-1);
+  const [userSelections, setUserSelections] = useState({});
 
   useEffect(() => {
     const newConnection = new HubConnectionBuilder()
@@ -24,14 +25,62 @@ const App = () => {
           connection.on('ReceiveTimerCountdown', (secondsUntil) => {
             setTimeUntil(secondsUntil);
           });
+
+          connection.on('ReceiveSelection', (userSelections) => {
+            setUserSelections(userSelections);
+          });
         })
         .catch((e) => console.log('Connection failed: ', e));
     }
   }, [connection]);
 
+  const userId = 'one';
+
+  const [selection, setSelection] = useState(null);
+
+  const handleUpdateSelection = (e) => {
+    setSelection(e.target.value);
+  };
+
+  const makeSelection = async () => {
+    // call endpoint
+    const userSelection = {
+      Id: userId,
+      Selection: selection,
+    };
+    console.log(JSON.stringify(userSelection));
+    try {
+      await fetch('http://localhost:5103/timer/select', {
+        method: 'POST',
+        body: JSON.stringify(userSelection),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (e) {
+      console.log('Sending message failed.', e);
+    }
+    setSelection('');
+  };
+
   return (
     <>
       <h1>{timeUntil}</h1>
+      <>
+        <h3>user: {userId}</h3>
+        <input onChange={handleUpdateSelection} value={selection} />
+        <button onClick={makeSelection}>send</button>
+      </>
+      <p>
+        Current selections
+        {Object.keys(userSelections).map((k) =>
+          userSelections[k].map((sel) => (
+            <p>
+              {k}: {sel}
+            </p>
+          ))
+        )}
+      </p>
     </>
   );
 };
